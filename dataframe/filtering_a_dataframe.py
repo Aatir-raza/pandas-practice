69f09bb1d0081e6593c713aa676f2affc6d7e7e9
Aatir Raza <syedaatir77@gmail.com>
2026-07-28 23:36:43 +0530

    add some question

--- dataframe/filtering_a_dataframe.py
+++ dataframe/filtering_a_dataframe.py
@@ -1,5 +1,19 @@
+# find all the final winners
 import pandas as pd 
 a=(pd.read_csv(r"C:\Users\Homes\Desktop\PANDAS\dataframe\ipl-matches.csv"))
 mask=a['MatchNumber']=='Final'
 new_df=a[mask]
 print(new_df[['Season','WinningTeam']])
+
+# how many super over finishes have occured
+b=a[a['SuperOver']=='Y'].shape[0]
+print(b)
+
+# how many matches has csk won in kolkata
+c=a[(a['City']=='Kolkata') & (a['WinningTeam'] == 'Chennai Super Kings')].shape
+print(c)
+ # toss winner is match winner in percentage
+A=(a[a['TossWinner']== a['WinningTeam']].shape[0]/a.shape[0])*100
+print(A)
+
+
